751739dee86357efdf3a76c0546ffcdac074030f
CorretorBrazza <corretorbrazza@gmail.com>
2026-07-12 17:01:10 -0300

    chore: liberação de conexões externas CORS no main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // <-- ADICIONE ESTA LINHA para liberar a conexão do PWA de forma segura
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
